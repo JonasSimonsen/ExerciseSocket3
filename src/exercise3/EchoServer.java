1bf58030b9baa7fdd2ed3cfb40cc67505cd5edd5
jonassimonsen <jonassimonsen@10.50.137.12>
2015-08-28 13:03:30 +0200

    First Commit

--- src/exercise3/EchoServer.java
+++ src/exercise3/EchoServer.java
@@ -22,45 +22,10 @@ import java.util.logging.Logger;
 public class EchoServer implements Runnable {
 
     Socket s;
+    PrintWriter out;
     BufferedReader in;
     String echo;
-    PrintWriter out;
-    String replyMessage;
-
-    public EchoServer(Socket soc) {
-        s = soc;
-    }
-
-    @Override
-    public void run() {
-        try {
-            in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-            out = new PrintWriter(s.getOutputStream(), true);
-            while (true) {
-                echo = in.readLine();
-                switch (echo) {
-                    case "UPPER#Hello World":
-                        replyMessage = "HELLO WORLD";
-                        break;
-                    case "LOWER#Hello World":
-                        replyMessage = "hello world";
-                        break;
-                    case "REVERSE#abcd":
-                        replyMessage = "dcba";
-                        break;
-                    case "TRANSLATE#hund":
-                        replyMessage = "dog";
-                        break;
-
-                    default:
-                        s.close();
-                }
-                out.println(replyMessage);
-            }
-        } catch (IOException ex) {
-            Logger.getLogger(EchoServer.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+    String reply;
 
     public static void main(String[] args) throws IOException {
         String ip = "localhost";
@@ -69,12 +34,68 @@ public class EchoServer implements Runnable {
             ip = args[0];
             port = Integer.parseInt(args[1]);
         }
+
         ServerSocket ss = new ServerSocket();
         ss.bind(new InetSocketAddress(ip, port));
+
         while (true) {
             EchoServer e = new EchoServer(ss.accept());
+            String echo;
             Thread t1 = new Thread(e);
             t1.start();
         }
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                echo = in.readLine();
+
+                out = new PrintWriter(s.getOutputStream(), true);
+
+                Command(echo);
+
+                out.println(reply);
+            } catch (IOException ex) {
+                Logger.getLogger(EchoServer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    private String Command(String echo) {
+        reply = echo;
+        String[] splitter = echo.split("#");
+
+        switch (splitter[0]) {
+            case "UPPER":
+                reply = splitter[1].toUpperCase();
+                break;
+            case "LOWER":
+                reply = splitter[1].toLowerCase();
+                break;
+            case "REVERSE":
+                reply = new StringBuilder(splitter[1]).reverse().toString();
+                break;
+            case "TRANSLATE":
+                if (splitter[1].equals("hund")) {
+                    reply = "dog";
+                    return reply;
+                }
+                if (splitter[1].equals("kat")) {
+                    reply = "cat";
+                    return reply;
+                }
+                reply = "#NOT FOUND";
+                break;
+            default:
+                break;
+        }
+        return reply;
+    }
+
+    private EchoServer(Socket socket) {
+        s = socket;
     }
 }
